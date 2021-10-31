cba96dc2bebf47600d20ca01eb4256eae8dd141a
Leona Meharenna <lmeha2@uic.edu>
2021-10-31 14:41:14 -0500

    Initial Commit

---
+++ 312PantryStarter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  312PantryStarter
+//
+//  Created by Leona  Meharenna on 10/31/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
