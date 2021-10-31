974d288aafd2f1ec2db751e4381559f34c3a1c39
Leona Meharenna <lmeha2@uic.edu>
2021-10-31 15:01:45 -0500

    Starter template. PLEASE ADD YOUR CODE TO THE FILE THEY CORRESPOND TO. i.e. grace adds her code to the loginVC, brenny bren bren adds her code to the upload vc, etc

---
+++ 312PantryStarter/AugRealVC.swift
@@ -0,0 +1,29 @@
+//
+//  AugRealVC.swift
+//  312PantryStarter
+//
+//  Created by Leona  Meharenna on 10/31/21.
+//
+
+import UIKit
+
+class AugRealVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
